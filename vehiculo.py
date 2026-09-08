8d6dffec6cc445522bec42d8434543244065097b
JosemaVer <jmavergara15@gmail.com>
2026-09-07 22:18:00 -0300

    Agrega metodos ingresar y entregar a la clase Vehiculo

--- vehiculo.py
+++ vehiculo.py
@@ -8,3 +8,13 @@ class Vehiculo:
         self.anio: int = anio
         # Inicializacion automatica del atributo privado _en_taller en False
         self._en_taller: bool = False
+
+    # Metodo para registrar el ingreso del vehiculo al taller
+    def ingresar(self) -> None:
+        # Cambia el estado de _en_taller a True indicando que el vehiculo esta en el taller
+        self._en_taller = True
+
+    # Metodo para registrar la entrega del vehiculo al cliente
+    def entregar(self) -> None:
+        # Cambia el estado de _en_taller a False indicando que el vehiculo salio del taller
+        self._en_taller = False
