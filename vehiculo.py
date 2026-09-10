4622aa81796bb480278fe884fdede18dcc64185f
JosemaVer <jmavergara15@gmail.com>
2026-09-09 22:05:15 -0300

    Implementacion de clases de vehiculos y programa principal

--- vehiculo.py
+++ vehiculo.py
@@ -10,11 +10,18 @@ class Vehiculo:
         self._en_taller: bool = False
 
     # Metodo para registrar el ingreso del vehiculo al taller
-    def ingresar(self) -> None:
+    def ingresar_al_taller(self) -> None:
         # Cambia el estado de _en_taller a True indicando que el vehiculo esta en el taller
         self._en_taller = True
 
     # Metodo para registrar la entrega del vehiculo al cliente
-    def entregar(self) -> None:
+    def entregar_al_cliente(self) -> None:
         # Cambia el estado de _en_taller a False indicando que el vehiculo salio del taller
         self._en_taller = False
+
+    # Metodo que retorna la tarifa por hora generica del vehiculo
+    def tarifa_hora(self) -> int:
+        return 5000
+
+    def patente_v(self) -> str:
+        return self.patente
